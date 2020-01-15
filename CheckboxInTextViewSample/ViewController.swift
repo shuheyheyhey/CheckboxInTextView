7754b150d3a02f9195b3bd15d662ae601dfe241f
Shuhei Yukawa <shuhei.yukawa@l-is-b.com>
2020-01-15 15:22:04 +0900

    Initial Commit

---
+++ CheckboxInTextViewSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CheckboxInTextViewSample
+//
+//  Created by Shuhei Yukawa on 2020/01/15.
+//  Copyright © 2020 Shuhei Yukawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
